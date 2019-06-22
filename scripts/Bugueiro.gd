9483cc977c5ca904ebe5112eb8471374a52be65a
Alllv <alllvgiant@gmail.com>
2019-06-22 18:33:16 -0300

    Ajuste no angulo do carro

--- scripts/Bugueiro.gd
+++ scripts/Bugueiro.gd
@@ -7,6 +7,7 @@ const FLOOR = Vector2(0, -1)
 onready var timer = get_node("Timer")
 
 var velocity = Vector2()
+var first_collision = false
 
 func _ready():
 	$buggyFx.play()
@@ -22,13 +23,25 @@ func _physics_process(delta):
 	velocity.x = 2*SPEED
 	velocity.y += GRAVITY
 	
-	velocity = move_and_slide(velocity, FLOOR)
+	if first_collision == false:
+		velocity = move_and_slide(velocity, FLOOR)
+	else:
+		var collision_info = move_and_collide(velocity * delta, true, true, true)
+		if collision_info:
+			var collision_normal = collision_info.normal	
+			var new_angle = rad2deg(collision_normal.angle_to_point(velocity * delta)) + 35 # << 35 is a random number, I tried others but this works better
+			rotation_degrees = new_angle;
+			print(new_angle)
+		velocity = move_and_slide(velocity, FLOOR)
 	pass
 
 func _on_Area2D_body_entered(body):
 	if(body.get_name() == "Player"):
 		body.anim = "Falling"
 		queue_free()
+	else:
+		if typeof(body) == 17:
+			first_collision = true
 		
 func _on_timer_timeout():
 		print("destruiu")
